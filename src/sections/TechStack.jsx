3d6c252f3b5da684d42fa19877d05c51828a1866
unknown <mayur7024k@gmail.com>
2025-06-10 01:04:11 +0530

    lib change

--- src/sections/TechStack.jsx
+++ src/sections/TechStack.jsx
@@ -2,7 +2,9 @@ import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 
 import TitleHeader from "../components/TitleHeader";
-import TechIconCardExperience from "../components/models/tech_logos/TechIconCardExperience";
+// import TechIconCardExperience from "../components/models/tech_logos/TechIconCardExperience";
+import TechIconCardExperience from '../components/models/tech_logos/TechIconCardExperience.jsx';
+
 import { techStackIcons } from "../constants";
 // import { techStackImgs } from "../constants";  // No longer needed since we're commenting out the image section
 
